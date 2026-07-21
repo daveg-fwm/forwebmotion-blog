6aef254c111870da6bff5eb0b3009f0e98ed9513
Dave Green <wddavegreen@gmail.com>
2026-07-22 00:11:34 +0200

    feat: install shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { cn } from "@/utils/_base/utils";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en" className={cn("h-full", "antialiased", "font-sans", inter.variable)}>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
