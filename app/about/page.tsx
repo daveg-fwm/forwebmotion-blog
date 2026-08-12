bb6d7f4878e733785e34fe06c862a865cc078812
Dave Green <wddavegreen@gmail.com>
2026-08-13 01:41:34 +0200

    fix: ts errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import { AspectRatio } from "@/components/_base/aspect-ratio/aspect-ratio";
 import { JsonLd } from "@/components/json-ld/json-ld";
 import { OG_IMAGE_HEIGHT, OG_IMAGE_TYPE, OG_IMAGE_WIDTH, SITE_NAME } from "@/constants/constants";
-import ProfilePhoto from "@/public/images/Dave-Green-profile-photo.jpg";
 import { getAboutJsonLd } from "@/utils/json-ld/json-ld";
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -62,12 +61,11 @@ export default async function AboutPage() {
           className="w-full max-w-72 overflow-hidden rounded-2xl lg:relative lg:-top-10 lg:w-72 lg:shrink-0"
         >
           <Image
-            src={ProfilePhoto}
+            src="/images/Dave-Green-profile-photo.jpg"
             alt={content.imageAlt}
             fill
             sizes="288px"
             className="object-cover"
-            placeholder="blur"
             priority
           />
         </AspectRatio>
